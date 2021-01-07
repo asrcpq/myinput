01011acf90ec52407338bfee21e95d5639dc2de5
asrcpq <asrcpq@outlook.com>
2021-01-07 15:47:15 +0000

    use hpm

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
+stage0() {
+	cd $(dirname "$0")
+	git clone --depth=1 https://github.com/google/mozc
+}
+
 stage1() {
-	MOZC_DIR="$HOME/git/google/mozc/src/data/"
+	MOZC_DIR="./mozc/src/data/"
 	cat $MOZC_DIR/dictionary_oss/dictionary*.txt | cut -d'	' -f1,5 > dict.out.txt
 	python3 stage1_sk.py "$MOZC_DIR/single_kanji/single_kanji.tsv" >> dict.out.txt
 }
@@ -9,5 +14,6 @@ stage2() {
 	cat map.txt >> stage2.out.txt
 }
 
+stage0
 stage1
 stage2
