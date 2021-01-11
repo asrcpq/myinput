881e9eacd56512fbd0ad1d8adecc43f8ddbdf268
asrcpq <asrcpq@outlook.com>
2021-01-11 07:35:35 +0000

    add sunpinyin

--- build.sh
+++ build.sh
@@ -2,7 +2,13 @@ set -e
 
 stage0() {
 	cd $(dirname "$0")
-	git clone --depth=1 https://github.com/google/mozc
+	mkdir -p dicts
+	if ! [ -d ./mozc ]; then
+		git clone --depth=1 https://github.com/google/mozc
+	fi
+	curl -sL https://raw.githubusercontent.com/sunpinyin/open-gram/master/data/dict.full | \
+		tr -d "'" | \
+		awk '{print $2, $1}' > dicts/sunpinyin.out.txt
 }
 
 stage1() {
@@ -12,10 +18,10 @@ stage1() {
 }
 
 stage2() {
-	python3 stage2.py | awk '!x[$0]++' > stage2.out.txt
-	cat map.txt >> stage2.out.txt
+	python3 stage2.py | awk '!x[$0]++' > dicts/mozc.out.txt
+	cat map.txt >> dicts/mozc.out.txt
 }
 
 stage0
-stage1
-stage2
+# stage1
+# stage2
